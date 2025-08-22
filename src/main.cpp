9503610bb5f87926d1712d0aacb219260226376a
Florent <florent@frizoncorrea.fr>
2025-08-22 18:03:13 +0200

    adding serial gw example to build out of tree

---
+++ src/main.cpp
@@ -0,0 +1,168 @@
+#include "SensorMesh.h"
+
+class MyMesh : public SensorMesh {
+public:
+  MyMesh(mesh::MainBoard& board, mesh::Radio& radio, mesh::MillisecondClock& ms, mesh::RNG& rng, mesh::RTCClock& rtc, mesh::MeshTables& tables)
+     : SensorMesh(board, radio, ms, rng, rtc, tables), 
+       battery_data(12*24, 5*60)    // 24 hours worth of battery data, every 5 minutes
+  {
+  }
+
+protected:
+  /* ========================== custom logic here ========================== */
+  Trigger low_batt, critical_batt, serial;
+  TimeSeriesData  battery_data;
+
+  void onSensorDataRead() override {
+    float batt_voltage = getVoltage(TELEM_CHANNEL_SELF);
+
+    battery_data.recordData(getRTCClock(), batt_voltage);   // record battery
+    alertIf(batt_voltage < 3.4f, critical_batt, HIGH_PRI_ALERT, "Battery is critical!");
+    alertIf(batt_voltage < 3.6f, low_batt, LOW_PRI_ALERT, "Battery is low");
+  }
+
+  int querySeriesData(uint32_t start_secs_ago, uint32_t end_secs_ago, MinMaxAvg dest[], int max_num) override {
+    battery_data.calcMinMaxAvg(getRTCClock(), start_secs_ago, end_secs_ago, &dest[0], TELEM_CHANNEL_SELF, LPP_VOLTAGE);
+    return 1;
+  }
+
+  bool handleCustomCommand(uint32_t sender_timestamp, char* command, char* reply) override {
+    if (strcmp(command, "magic") == 0) {    // example 'custom' command handling
+      strcpy(reply, "**Magic now done**");
+      return true;   // handled
+    } else if (memcmp(command, "sout ", 5) == 0) {
+      SERIAL_GW.println(&command[5]);
+      return true;
+    }
+    return false;  // not handled
+  }
+
+public:
+  void loop() {
+    SensorMesh::loop();
+
+#ifdef SERIAL_GW
+    static char in_data[156];
+    static char out_data[160] = "s> ";
+
+    int len = strlen(in_data);
+    while (SERIAL_GW.available() && len < 155) {
+      char c = SERIAL_GW.read();
+      if (c != '\n') {
+        in_data[len++] = c;
+        in_data[len] = 0;
+      }
+    }
+    if (len == 155) {  // buffer full ... send
+      in_data[155] = '\r';
+    }
+
+    if (len > 0 && in_data[len - 1] == '\r') {  // received complete line
+      serial.text[0] = 0; // retrigger serial alert
+      in_data[len - 1] = 0;  // replace newline with C string null terminator
+      strncpy(&out_data[3], in_data, 156);
+      alertIf(true, serial, LOW_PRI_ALERT, out_data);
+
+      in_data[0] = 0;  // reset buffer
+    }
+#endif
+  }
+  /* ======================================================================= */
+};
+
+StdRNG fast_rng;
+SimpleMeshTables tables;
+
+MyMesh the_mesh(board, radio_driver, *new ArduinoMillis(), fast_rng, rtc_clock, tables);
+
+void halt() {
+  while (1) ;
+}
+
+static char command[160];
+
+void setup() {
+  Serial.begin(115200);
+
+#ifdef SERIAL_GW
+  SERIAL_GW.setRx(SGW_RX);
+  SERIAL_GW.setTx(SGW_TX);
+  SERIAL_GW.begin(115200);
+#endif
+
+  delay(1000);
+
+  board.begin();
+
+  if (!radio_init()) { halt(); }
+
+  fast_rng.begin(radio_get_rng_seed());
+
+  FILESYSTEM* fs;
+#if defined(NRF52_PLATFORM) || defined(STM32_PLATFORM)
+  InternalFS.begin();
+  fs = &InternalFS;
+  IdentityStore store(InternalFS, "");
+#elif defined(ESP32)
+  SPIFFS.begin(true);
+  fs = &SPIFFS;
+  IdentityStore store(SPIFFS, "/identity");
+#elif defined(RP2040_PLATFORM)
+  LittleFS.begin();
+  fs = &LittleFS;
+  IdentityStore store(LittleFS, "/identity");
+  store.begin();
+#else
+  #error "need to define filesystem"
+#endif
+  if (!store.load("_main", the_mesh.self_id)) {
+    MESH_DEBUG_PRINTLN("Generating new keypair");
+    the_mesh.self_id = radio_new_identity();   // create new random identity
+    int count = 0;
+    while (count < 10 && (the_mesh.self_id.pub_key[0] == 0x00 || the_mesh.self_id.pub_key[0] == 0xFF)) {  // reserved id hashes
+      the_mesh.self_id = radio_new_identity(); count++;
+    }
+    store.save("_main", the_mesh.self_id);
+  }
+
+  Serial.print("Sensor ID: ");
+  mesh::Utils::printHex(Serial, the_mesh.self_id.pub_key, PUB_KEY_SIZE); Serial.println();
+
+  command[0] = 0;
+
+  sensors.begin();
+
+  the_mesh.begin(fs);
+
+  // send out initial Advertisement to the mesh
+  the_mesh.sendSelfAdvertisement(16000);
+}
+
+void loop() {
+  int len = strlen(command);
+  while (Serial.available() && len < sizeof(command)-1) {
+    char c = Serial.read();
+    if (c != '\n') {
+      command[len++] = c;
+      command[len] = 0;
+    }
+    Serial.print(c);
+  }
+  if (len == sizeof(command)-1) {  // command buffer full
+    command[sizeof(command)-1] = '\r';
+  }
+
+  if (len > 0 && command[len - 1] == '\r') {  // received complete line
+    command[len - 1] = 0;  // replace newline with C string null terminator
+    char reply[160];
+    the_mesh.handleCommand(0, command, reply);  // NOTE: there is no sender_timestamp via serial!
+    if (reply[0]) {
+      Serial.print("  -> "); Serial.println(reply);
+    }
+
+    command[0] = 0;  // reset command buffer
+  }
+
+  the_mesh.loop();
+  sensors.loop();
+}
