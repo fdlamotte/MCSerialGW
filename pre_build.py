2414b0c3aa22e51a3a1559c5bc6ec19b114016d9
Florent <florent@frizoncorrea.fr>
2025-08-24 18:27:30 +0200

    getting rid of libraries in arch

--- pre_build.py
+++ pre_build.py
@@ -4,19 +4,35 @@ Import ("env") # type: ignore
 menv = env    # type: ignore
 
 env_name = menv["PIOENV"]
+variant_name = None
+
+stm32_platform = False
+esp32_platform = False
+nrf52_platform = False
+rp2040_platform = False
 
 # add variant dir from MeshCore tree in libdeps to includes
 for item in menv.get("BUILD_FLAGS", []):
     if "MC_VARIANT" in item :
         variant_name = item.split("=")[1]
-        menv.Append(BUILD_FLAGS=[f"-I .pio/libdeps/{env_name}/MeshCore/variants/{variant_name}"])
+        variant_dir = f".pio/libdeps/{env_name}/MeshCore/variants/{variant_name}"
+        menv.Append(BUILD_FLAGS=[f"-I {variant_dir}"])
+    elif "STM32_PLATFORM" in item :
+        stm32_platform = True
 
 # add advert name from PIOENV
 menv.Append(BUILD_FLAGS=[f"-D ADVERT_NAME=\'\"{env_name}\"\'"])
 
-# copy ed25519 in .pio/libdeps
+# copy libs from MC
 libdeps  =f".pio/libdeps/{env_name}/"
 mc_dir = libdeps+"MeshCore/"
+
+# copy ed25519 in .pio/libdeps
 ed_dir = libdeps+"ed25519/"
 if not os.path.exists(ed_dir):
     shutil.copytree(mc_dir+"lib/ed25519", ed_dir)
+
+# if STM32_PLATFORM, then get LFS wrapper
+lfs_dir = libdeps+"Adafruit_LittleFS_stm32/"
+if stm32_platform and not os.path.exists(lfs_dir):
+    shutil.copytree(mc_dir+"arch/stm32/Adafruit_LittleFS_stm32", lfs_dir)
