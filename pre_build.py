a1ecb55caba801903714a37573d42b31fdd25e40
Florent <florent@frizoncorrea.fr>
2025-08-24 12:44:54 +0200

    ignore pylance errors

--- pre_build.py
+++ pre_build.py
@@ -1,12 +1,13 @@
-Import ("env")
+Import ("env") # type: ignore
+menv = env    # type: ignore
 
-env_name = env["PIOENV"]
+env_name = menv["PIOENV"]
 
 # add variant dir from MeshCore tree in libdeps to includes
-for item in env.get("BUILD_FLAGS", []):
+for item in menv.get("BUILD_FLAGS", []):
     if "MC_VARIANT" in item :
         variant_name = item.split("=")[1]
-        env.Append(BUILD_FLAGS=[f"-I .pio/libdeps/{env_name}/MeshCore/variants/{variant_name}"])
+        menv.Append(BUILD_FLAGS=[f"-I .pio/libdeps/{env_name}/MeshCore/variants/{variant_name}"])
 
 # add advert name from PIOENV
-env.Append(BUILD_FLAGS=[f"-D ADVERT_NAME=\'\"{env_name}\"\'"])
+menv.Append(BUILD_FLAGS=[f"-D ADVERT_NAME=\'\"{env_name}\"\'"])
